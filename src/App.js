1a44d8fd1a0f9ae6a4c285ad5f1583412432d1e5
rfreirez <renangfz@hotmail.com>
2022-10-30 20:46:05 -0500

    Carga de equipos del mundial en DB json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
 import React, { useEffect } from 'react';
+import {equiposMundial} from './equipos';
 
 async function render() {
+  // Variables y constantes
   let dataBack = {}
+  let payloadSesion =   {
+    "email": "example@gmail.com",
+    "password": "***"
+  }
+  // // Integración con la Api de FIFA
+  // // Logeo - Control de Tiempo de Sesion ?
+  // const dataSesionawait = await fetch('api/v1/user/login', {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify(payloadSesion)
+  // }).then(res => res.json());
+
+  // // Extrae todos los grupos 
+  // let gruposMundial = await fetch('api/v1/standings', {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": "Bearer "+dataSesionawait.data.token,
+  //     }
+  //   }).then(res => res.json());
+    let gruposMundial = equiposMundial[0];
+
+    let participanteArreglo = []
+    let participanteId = 0;
+    let tournamentId = 0;
+    let contadorEquipos = 0;
+    // Arma participantes
+    gruposMundial.data.forEach((grupoElemento) => {
+      contadorEquipos = 0;
+      grupoElemento.teams.forEach((equipoElemento) => {
+        if(contadorEquipos > 1) {
+          let participanteElemento = {};
+          participanteElemento.id = participanteId;
+          participanteElemento.tournament_id = tournamentId;
+          participanteElemento.name = equipoElemento.name_en;
+          participanteArreglo.push(participanteElemento)
+          participanteId = participanteId + 1;   
+        } 
+        contadorEquipos = contadorEquipos + 1;
+      }); 
+    });
+    // Alamcena participantes en db
+    await fetch('http://localhost:8081/participant', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(participanteArreglo)
+    }).then(res => res.json());
+
+  // ()()()()()()()()
+  // Se extrae de la base de datos json-server
   let dataStage = await fetch('http://localhost:8081/stage').then(res => res.json());
   let dataMatch = await fetch('http://localhost:8081/match').then(res => res.json());
   let dataParticipant = await fetch('http://localhost:8081/participant').then(res => res.json());
@@ -38,7 +94,8 @@ async function render() {
     // Obtiene nombre equipo seleccionado en base al estilo hover de la clase participant
     const equipoSeleccionadoHTML = document.querySelector(`[data-match-id="${matchObjeto.id}"] .opponents .participant:hover .name`).textContent;
     // Obtiene objeto de tipo Participant filtrado por nombre de equipo seleccionado
-    let equipoObjeto = await fetch('http://localhost:8081/participant?name=' + equipoSeleccionadoHTML).then(res => res.json());
+    // let equipoObjeto = await fetch('http://localhost:8081/participant?name=' + equipoSeleccionadoHTML).then(res => res.json());
+    let equipoObjeto = dataBack.participant.filter(e => e.name === equipoSeleccionadoHTML)
     // En base al match captado en el evento onMatchClicked
     // determinar a que oponente se hizo la seleccion para colocar estado de win y loss
     if(equipoObjeto[0].id === matchObjeto.opponent1.id){
@@ -62,18 +119,18 @@ async function render() {
     const resultDataTorneo = await window.bracketsManager.export();
     console.log("resultDataTorneo");
     console.log(resultDataTorneo);
-    // Actualizamos base de datos de match
+    // Actualizamos base de datos de match    
     for (const singleMatch of resultDataTorneo.match) {
-      await fetch('http://localhost:8081/match/'+ singleMatch.id, {
+    // resultDataTorneo.match.forEach(async (singleMatch) => {
+      await fetch('http://localhost:8081/match/' + singleMatch.id, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json"
         },
         body: JSON.stringify(singleMatch)
-      }).then(res => res.json());
-        
-    }
-    
+      }).then(res => res.json());        
+    // });
+    }    
     render();
   }
 
